08f431fb8484f9520e9949d23e2a13c4677dd66e
Gulzada IISAEVA <iisaevagulzada@gmail.com>
2017-01-19 23:21:40 +0200

    Add files via upload

---
+++ Computer.h
@@ -0,0 +1,52 @@
+/* 
+ * File:   Computer.h
+ * Author: Gulzada IISAEVA - 131044085
+ * 
+Belli  bir kurallari olan programlama dilinin bilgilerinin bulundugu text
+dosyasi vardir.Dosyadan okundugu bilgilerin o dilin kurallarina uygun  kodu
+yazilip verilen text dosyasi bi program gibi calismali. Programda 
+5 tane registr ve unsigned int [50] var. Dosyadan instructionlari okuyup
+bu registr ve memory uzerinden OOP kullanilarak islemler yapilacak 
+ *   
+/*Bu class onceden olusturulan CPUProgram CPU Memory obejelerini alip kendi 
+ * olusturdugu objlere atiyor ve uzerinden  islemleri yapiyor */
+ 
+
+
+#ifndef COMPUTER_H
+#define	COMPUTER_H
+#include<iostream>
+#include<string>
+#include<fstream>
+
+#include "Memory.h"
+#include "CPU.h"
+#include "CPUProgram.h"
+
+using namespace std;
+
+class Computer {
+public:
+    //Bu constructor maindeki classlarin objelerini alip yeni olusturulan objelere atiyor
+    Computer(CPU myCPU,CPUProgram myCPUProgram,Memory myMemory,const int option);
+    Computer(const int option);
+   
+    CPU getCPU() {return computerCPU;};
+    CPUProgram getCPUProgram() {return computerCPUProgram;};
+    Memory getMemory() {return computerMemory;};
+    
+    void setCPU(CPU myCPU) {computerCPU=myCPU;};
+    void setCPUProgram(CPUProgram myCPUProgram) {computerCPUProgram=myCPUProgram;};
+    void setMemory(Memory myMemory) {computerMemory=myMemory;};
+
+    //dosyadan okuyup butun instructionlari teker teker hlt gorene kadar calistiryor
+    void execute();
+private:
+    CPU computerCPU;
+    CPUProgram computerCPUProgram;
+    Memory computerMemory;
+    int option;
+};
+
+#endif	/* COMPUTER_H */
+
